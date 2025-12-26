1564d81eb7b2c555144a3392835d954495e633f4
jltz1207 <lee.jasonhk@gmail.com>
2025-12-26 22:24:46 +0800

    updated .gitignore

---
+++ LoginDotnet/Extensions/ServicesExtension.cs
@@ -0,0 +1,24 @@
+﻿using LoginDotnet.Data;
+
+namespace LoginDotnet.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void AddApplicationService(ref IServiceCollection service, IConfiguration config)
+        {
+
+            service.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            service.AddDbContext<ApplicationContext>(
+                options => options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+                );
+            service.AddOpenApi();
+
+            service.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            return;
+        }
+        
+    }
+}
--- LoginDotnet/Program.cs
+++ LoginDotnet/Program.cs
@@ -1,22 +1,10 @@
 using LoginDotnet.Data;
+using LoginDotnet.Extensions;
 using LoginDotnet.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddDbContext<ApplicationContext>(
-    options => options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
-    );
-builder.Services.AddOpenApi();
-builder.Services.AddIdentityCore<User>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-    options.Password.RequireDigit = true;
-})
-    .AddEntityFrameworkStores<ApplicationContext>();
+ServicesExtension.AddApplicationService(ref builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

322194bea62154c31a1f335c7d6b7897c712c431
DESKTOP-61UKK4R\User <lee.jasonhk@gmail.com>
2025-12-26 19:59:12 +0800

    init

---
+++ LoginDotnet/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace LoginDotnet.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+      
+    }
+}
